0951b505e7c2f0d2d701837049dddcd6ad63ffb2
Laurids Stockert <63673809+postnubilaphoebus@users.noreply.github.com>
2021-07-12 18:16:50 +0200

    Final Arnold Tongue

--- arnold_tongue.py
+++ arnold_tongue.py
@@ -1,40 +1,52 @@
 import numpy as np
-import math as m
+from math import floor, pow, sin, pi, ceil
 from random import uniform
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 import seaborn as sns 
+import datetime
 
-sns.color_palette("viridis", as_cmap=True)
-viridis = cm.get_cmap('viridis', 12)
 iter = 20
 n = 200
-omega_list = np.arange(0, 1.002, 0.002)
-K_list = np.arange(0, 1.002, 0.002)
-results = np.zeros(shape=(501,501))
-print("starting...")
+precision = 0.002
+omega_list = np.arange(0 + precision, 1 + precision, precision)
+K_list = np.arange(0 + precision, 1 + precision, precision)
+resolution = int(1 / precision)
+results = np.zeros(shape=(resolution,resolution))
+theta_list = np.arange(0, 1, 1/iter)
+
+print("Starting circle map computation...")
+est_time = ceil(7 * pow(10, -7) * pow(resolution, 2) *  iter * n)
+print("Starting time:", datetime.datetime.now().time())
+
+print("Resolution: {} x {}, Precision: {}, n: {}, Number of initial theta conditions: {}".format(resolution, resolution, precision, n, iter))
+if est_time < 60:
+    print("Estimated time: {} seconds".format(est_time))
+elif est_time < 3600:
+    print("Estimated time: {} minute(s)".format(ceil(est_time / 60)))
+else:
+    hours = round((est_time / 3600), 2)
+    hour = floor(hours)
+    minute = ceil((hours % 1) * 60)
+    print("Estimated time: {} hour(s) and {} minute(s)".format(hour, minute))
 
 for p, omega in enumerate(omega_list):
     for q, K in enumerate(K_list):
         W_mean = 0
-        for i in range(iter):
-            theta_n = uniform(0, 1)
+        for theta_n in theta_list:
+            theta_n = (theta_n + uniform(-0.02, 0.02)) % 1
             W = 0
             for j in range(n):
                 theta_before = theta_n
-                theta_n = (theta_n + omega - K * m.sin(2 * m.pi * theta_n)) 
+                theta_n = (theta_n + omega - K * sin(2 * pi * theta_n)) 
                 delta_theta = (theta_n - theta_before) % 1
                 W = W + delta_theta
             W = W / n
             W_mean = W_mean + W
         W_mean = W_mean / iter
-        results[p][q] = W_mean
-print("Done")
-sns.heatmap(results, cmap='viridis')
+        results[-q][p] = W_mean
+
+#pyplot.figure(figsize=(13, 13)) #uncomment if you want a larger figure
+pic = sns.heatmap(results, cmap='mako', cbar=False)
+pic.set(xticks=[])
+pic.set(yticks=[])
 plt.show()
-            
-        
-
-
-
